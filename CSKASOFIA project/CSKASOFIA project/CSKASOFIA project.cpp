95dc078dfdc920c7a26369e4d9ecd71394c155b1
Todor Atanasov <TYAtanasov21@codingburgas.bg>
2023-10-23 18:40:17 +0300

    Add raylib

--- CSKASOFIA project/CSKASOFIA project/CSKASOFIA project.cpp
+++ CSKASOFIA project/CSKASOFIA project/CSKASOFIA project.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "raylib.h"
 int main()
 {
-	std::cout << "Hallo World!";
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "basic window");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();   
 }
