6eed46fd9622555dcee532adba08ad44bc81f015
Todor Atanasov <TYAtanasov21@codingburgas.bg>
2023-10-23 20:07:42 +0300

    Add logo

--- CSKASOFIA project/CSKASOFIA project/CSKASOFIA project.cpp
+++ CSKASOFIA project/CSKASOFIA project/CSKASOFIA project.cpp
@@ -4,8 +4,9 @@ int main()
 {
     const int screenWidth = 800;
     const int screenHeight = 450;
-
+    Image logo = LoadImage("images/Logo.png");
     InitWindow(screenWidth, screenHeight, "basic window");
+    SetWindowIcon(logo);
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
