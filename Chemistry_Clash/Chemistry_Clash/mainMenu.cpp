3d1d4e8e4a5f2597d3bcfa15f15773d4241ee323
Todor Atanasov <TYAtanasov21@codingburgas.bg>
2023-11-02 23:13:21 +0200

    Add assets

--- Chemistry_Clash/Chemistry_Clash/mainMenu.cpp
+++ Chemistry_Clash/Chemistry_Clash/mainMenu.cpp
@@ -2,7 +2,7 @@
 #include "func.h"
 
 
-
+Image logo = LoadImage("../assets/Logo.png");
 mainMenu::mainMenu() {
     InitWindow(screenWidth, screenHeight, "Chemistry Clash");
     SetWindowIcon(logo);
@@ -11,8 +11,7 @@ mainMenu::mainMenu() {
     {
         BeginDrawing();
         ClearBackground(DARKGRAY);
-
- 
+        SetWindowIcon(logo);
                 makeButton({ (float)screenWidth / 2, (float)screenHeight / 2 }, "PLAY", 50, 200, 100, 5, LIGHTGRAY, BLACK, GRAY);
         EndDrawing();
     }
