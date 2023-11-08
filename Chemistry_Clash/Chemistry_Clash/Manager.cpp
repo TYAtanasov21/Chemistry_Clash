99b8a73ddfa8529fa1515e4d461c3de1a1ca3cf6
MTNenkov21 <107104836+MTNenkov21@users.noreply.github.com>
2023-11-08 16:37:06 +0200

    Add main menu butons

--- Chemistry_Clash/Chemistry_Clash/Manager.cpp
+++ Chemistry_Clash/Chemistry_Clash/Manager.cpp
@@ -21,8 +21,8 @@ void Manager::Initialize()
 void Manager::Start()
 {
     std::vector<Button> test ={
-    { {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 200, 100}, "PLAY", 50, GRAY, BLACK },
-    { {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2+ 200, 200, 100}, "PLAY", 50, GRAY, BLACK },
+    { {(SCREEN_WIDTH - 300) / 2,  200, 300, 100}, "PLAY", 50, GRAY, BLACK },
+    { {(SCREEN_WIDTH - 300) / 2, SCREEN_HEIGHT- 300, 300, 100}, "SETTINGS", 50, GRAY, BLACK },
 };
     scene->InitButtonList(test);
     InitScene(scene, scene->GetButtonList(), LIGHTGRAY);
